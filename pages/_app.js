046655c6baf88c0b980a94cb525a544ce7770597
Thibaud Veyssade <tveyssade@gmail.com>
2023-11-24 10:57:06 +0100

    lol

---
+++ pages/_app.js
@@ -0,0 +1,40 @@
+import '../styles/globals.css';
+import Head from 'next/head';
+
+import { Provider } from 'react-redux';
+
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import user from '../reducers/user';
+
+const reducers = combineReducers({user});
+
+const persistConfig = { key: 'hackatweet', storage };
+
+const store = configureStore({
+  reducer: persistReducer(persistConfig, reducers),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
+});
+
+const persistor = persistStore(store);
+/*
+const store = configureStore({
+  reducer: { bookmarks, user },
+});*/
+
+function App({ Component, pageProps }) {
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Head>
+          <title>hackatweet</title>
+        </Head>
+        <Component {...pageProps} />
+        </PersistGate>
+    </Provider>
+  );
+}
+
+export default App;
