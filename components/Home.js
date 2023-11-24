d3597c2f6fc1203efa03b0000e6d2f4fdc17734c
Thibaud Veyssade <tveyssade@gmail.com>
2023-11-24 14:43:17 +0100

    useEffect

--- components/Home.js
+++ components/Home.js
@@ -3,19 +3,68 @@ import LastTweets from "./LastTweets";
 import Trends from "./Trends";
 import styles from "../styles/Home.module.css";
 import Tweet from "./Tweet";
+import { useEffect, useState } from "react";
 
 function Home() {
-  let lastTweets = <LastTweets isLiked={false} />;
-  let trends = <Trends isHashtag={false} />;
+
+  const [dbTweet, setDbTweet] = useState([]);
+  const [caractere, setCaractere] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:3000/tweet", {})
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.result) {
+          //console.log('data', data); 
+          setDbTweet(data.content);
+        }
+      });
+  }, []);
+
+  const handleInputChange = (e) => {
+    const inputValue = e.target.value;
+    if (inputValue.length <= 280) {
+      setCaractere(inputValue);
+    }
+  };
+
+  const addTweet = () => {
+    const contentTweet = document.querySelector('#idContent').value;
+    console.log("dans l addTweet: " + contentTweet);
+    fetch("http://localhost:3000/tweet", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ content: contentTweet }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.result) {
+        document.querySelector('#idContent').value = '';
+        }
+      });
+  }
+
+  const contentMap = dbTweet.map((data, i) => {
+    return <Tweet key={i} {...data} />;
+  });
 
   return (
     <div className={styles.homeContainer}>
       <div className={styles.left}></div>
       <div className={styles.middle}>
-        <div className={styles.right}>
-          <Tweet></Tweet>
+        <div className={styles.middleHeader}>
+          <input
+            id="idContent"
+            value={caractere}
+            onChange={handleInputChange}
+            placeholder="What's up ?"
+          ></input>
+          <button onClick={() => addTweet()}>Tweet</button>
+          <p>{caractere.length}/280</p>
         </div>
+        <div className={styles.middleDown}>{contentMap}</div>
       </div>
+      <div className={styles.right}></div>
     </div>
   );
 }
--- components/Tweet.js
+++ components/Tweet.js
@@ -1,75 +1,26 @@
 import styles from "../styles/Tweet.module.css";
-import { useEffect, useState } from "react";
 import React from "react";
+import Image from "next/image";
+import Logo from "../assets/pineapple.png"
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
-function Tweet() {
-  const [caractere, setCaractere] = useState("");
-  const [dbTweet, setDbTweet] = useState([]);
-
-  const handleInputChange = (e) => {
-    const inputValue = e.target.value;
-    if (inputValue.length <= 280) {
-      setCaractere(inputValue);
-    }
-  };
-
-  const addTweet = () => {
-    const contentTweet = document.querySelector('#idContent').value;
-    console.log(contentTweet);
-    fetch("http://localhost:3000/tweet", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ content: contentTweet }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.result) {
-        document.querySelector('#idContent').value = '';
-        }
-      });
-  }
-
-  fetch("http://localhost:3000/tweet", {
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.result) {
-          setDbTweet(data.content)
-        }
-      });
-
-  const newTweet = dbTweet.map((content, index) => (
-    <div key={index} className={styles.tweet}>
+function Tweet(props) {
+  return (
+    <div className={styles.tweet}>
       <div className={styles.tweetUp}>
-        <img src="../assets/pineapple.png" alt="pineapple" />
+        <Image width={25}
+        height={50} src={Logo} alt="pineapple" />
         <p>jeanclaude</p>
       </div>
       <div>
         <div>
-          <p>{content}</p>
+          <p>{props.content}</p>
           <FontAwesomeIcon icon={faHeart} />
         </div>
       </div>
     </div>
-  ));
-
-  return (
-    <>
-      <div className={styles.middleHeader}>
-        <input
-          id="idContent"
-          value={caractere}
-          onChange={handleInputChange}
-          placeholder="What's up ?"
-        ></input>
-        <button onClick={() => addTweet()}>Tweet</button>
-        <p>{caractere.length}/280</p>
-      </div>
-      <div className={styles.middleDown}>{newTweet}</div>
-    </>
   );
 }
 
